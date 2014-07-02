a60f5c81d16f1c05dd906791b5ccfb9a99fc2fd8
Matthew Reed <mattreed1890@gmail.com>
2014-07-01 22:06:21 -0500

    add lexer

--- lexer.rb
+++ lexer.rb
@@ -15,10 +15,71 @@ class Lexer
 
       if identifier = chunk[/\A(a-z)\w*)/,1]
         if KEYWORDS.include?(identifier)
-          
+          tokens << [identifier.upcase.to_sym, identifier]
+        else
+          tokens << [:IDENTIFIER, identifier] 
         end
+        
+        i += identifier.size
+
+      elsif constant = chunk[/\A(A-Z)\w*)/,1]
+        tokens << [:CONSTANT, constant]
+        i += constant.size
+
+      elsif number = chunk[/\A([0-9]+)/,1]
+        tokens << [:NUMBER, number.to_i]
+        i += number.size
+        
+      elsif string = chunk[/\A"(.*?)"/,1]
+        tokens << [:STRING, string]
+        i += string.size + 2
+
+      elsif indent = chunk[/\A\:\n( +)/m,1]
+        if indent.size <= current_indent
+          raise "Bad indent level, got #{indent.size} indents, " +
+            "expected > #{current_indent}"
+        end
+
+        current_indent = indent.size
+        indent_stack.push(current_indent)
+        tokens << [:INDENT, indent.size]
+        i += indent.size + 2
+
+      elsif indent = chunk[/\A\n( *)/m, 1]
+        if indent.size == current_indent
+          tokens << [:NEWLINE, "\n"]
+        elsif indent.size < current_indent
+          while indent.size < current_indent
+            indent_stack.pop
+            current_indent = indent_stack.first || 0
+            tokens << [:DEDENT, indent.size]
+          end
+          tokens = << [:NEWLINEm "\n"]
+        else
+          raise "Missing ':'"
+        end
+        i += indent.size + 1
+
+      elsif operator = chunk[/\A(\|\||&&|==|!=|<=|>=)/,1]
+        tokens << [operator, operator]
+        i += operator.size
+
+      elsif chunk.match(/\A /)
+        i += i
+
+      else
+        value = chunk[0,1]
+        tokens << [value, value]
+        i += 1
       end
+
     end
+
+    while indent = indent_stack.pop
+      tokens << [:DEDENT, indent_stack.first || 0]
+    end
+
+    tokens
 
   end
 end
